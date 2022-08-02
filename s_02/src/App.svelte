64e2ed5c8959ddb390524776d6f348e3623e7100
Rainey Schafer <schafer.rainey@gmail.com>
2022-08-02 10:48:00 -0500

    Add practice problems 1-2

---
+++ s_02/src/App.svelte
@@ -0,0 +1,38 @@
+<script>
+	let firstName = 'Jane'
+	let lastName = 'Doe'
+	let color = 'red'
+	let showText = false
+
+	$: name = firstName + ' ' + lastName
+
+	function toggle() {
+		color = color === 'red' ? 'green' : 'red'
+		showText = !showText
+	}
+
+</script>
+
+<main>
+	<h1 style="color:{color}">Hello {name}!</h1>
+	{#if showText}
+		<p>I'm glad you are here</p>
+	{/if}
+	<button on:click={toggle}>Click Me</button>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+</style>
